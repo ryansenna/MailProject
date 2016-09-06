3c1b10354354a3c7f50fe2eed741e8d9b931b0b7
Railanderson Sena <Railanderson Sena@192.168.2.4>
2016-09-05 20:47:54 -0400

    test, add comments and check the send with CC method with the teacher.

--- src/main/java/com/rsenna/business/Mail.java
+++ src/main/java/com/rsenna/business/Mail.java
@@ -42,11 +42,8 @@ public class Mail {
     private final String emailSendPwd; // must take in the user login.
     private final String emailReceive; // must take in the "to" field
     private final String emailReceivePwd;// test purposes.
-    private final String emailCC1 = "";// list of emails
-    private final String emailCC2 = "";// list of emails.
-
-    private String subject = "";// subject of the message sent.
-    private String content = "";// content of the message sent.
+    private String emailCC1;// list of emails
+    private String emailCC2;// list of emails.
 
     // You will need a folder with this name or change it to another
     // existing folder
@@ -54,7 +51,7 @@ public class Mail {
 
     public Mail(String smtpServerName, String imapServerName, String emailSend,
             String emailSendPwd, String emailReceive, String emailReceivePwd) {
-        
+
         this.smtpServerName = smtpServerName;
         this.imapServerName = imapServerName;
         this.emailSend = emailSend;
@@ -64,8 +61,16 @@ public class Mail {
 
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public Mail(Mail m, String emailCC1, String emailCC2) {
+        this.smtpServerName = m.getSmtpServerName();
+        this.imapServerName = m.getImapServerName();
+        this.emailSend = m.getEmailSend();
+        this.emailSendPwd = m.getEmailSendPwd();
+        this.emailReceive = m.getEmailReceive();
+        this.emailReceivePwd = m.getEmailReceivePwd();
+
+        this.emailCC1 = emailCC1;
+        this.emailCC2 = emailCC2;
     }
 
     /// ************** LIST OF GETTERS ***************
@@ -124,8 +129,9 @@ public class Mail {
      * Standard send routine using Jodd. Jodd knows about GMail so no need to
      * include port information
      */
-    public void sendEmail() {
+    public void sendEmail(String subject, String content) {
 
+        //VALIDATE BOTH PARAMS HERE.
         // Create am SMTP server object
         SmtpServer<SmtpSslServer> smtpServer = SmtpSslServer
                 .create(smtpServerName)
@@ -137,7 +143,7 @@ public class Mail {
         // Using the fluent style of coding create a plain text message
         Email email = Email.create().from(emailSend)
                 .to(emailReceive)
-                .subject(getSubject()).addText(getContent());
+                .subject(subject).addText(content);
 
         // A session is the object responsible for communicating with the server
         SendMailSession session = smtpServer.createSession();
@@ -150,40 +156,42 @@ public class Mail {
     }
 
     /**
-     * This Method will get the subject field from the user.
-     *
-     * @return subject
+     * *********** REVIEW THIS METHOD ************** Example with CC field and
+     * alternative to fluent style for adding CC
      */
-    public String getSubject() {
-        return subject;
-    }
+    public void sendEmailWithCC(String subject, String content) {
 
-    /**
-     * This method will get the Text field from the user.
-     *
-     * @return plainTxt
-     */
-    public String getContent() {
-        return content;
-    }
+        // VALIDATE SUBJECT HERE.
+        //VALIDATE CONTENT HERE.
+        // Create am SMTP server object
+        SmtpServer<SmtpSslServer> smtpServer = SmtpSslServer
+                .create(smtpServerName)
+                .authenticateWith(emailSend, emailSendPwd);
 
-    /**
-     * Whatever i receive from outside application will be put here.
-     *
-     * @param subject the subject of the email.
-     */
-    public void setSubject(String subject) {
-        if (subject.length() > 78) {
-            throw new IllegalArgumentException("The Length must be not above 78.");
+        // Display Java Mail debug conversation with the server
+        smtpServer.debug(true);
+
+        // Using the fluent style requires EmailMessage
+        Email email = Email.create().from(emailSend)
+                .to(emailReceive)
+                .cc(new EmailAddress[]{new EmailAddress(emailCC1),
+            new EmailAddress(emailCC2)})
+                .subject(subject).addText(content);
+
+        // Display all the cc addresses
+        MailAddress[] s = email.getCc();
+        for (MailAddress ma : s) {
+            log.info("cc: " + ma.getEmail());
         }
-        this.subject = subject;
-    }
 
-    /**
-     * Example with CC field and alternative to fluent style for adding CC
-     */
-    public void sendEmailWithCC() {
+        // A session is the object responsible for communicating with the server
+        SendMailSession session = smtpServer.createSession();
 
+        // Like a file we open the session, send the message and close the
+        // session
+        session.open();
+        session.sendMail(email);
+        session.close();
     }
 
     /**
@@ -191,7 +199,124 @@ public class Mail {
      * KenImapSslServer extends ImalSslServer and adds a property to display the
      * mail debug conversation
      */
-    public void receiveEmail() {
+  public void receiveEmail() {
+
+        // Create am IMAP server object
+        ImapSslServer imapSslServer = new ImapSslServer(imapServerName,
+                emailReceive, emailReceivePwd);
+
+        // Display the converstaion between the application and the imap server
+        imapSslServer.setProperty("mail.debug", "true");
+
+        // A session is the object responsible for communicating with the server
+        ReceiveMailSession session = imapSslServer.createSession();
+        session.open();
+
+        // We only want messages that have not be read yet.
+        // Messages that are delivered are then marked as read on the server
+        ReceivedEmail[] emails = session.receiveEmailAndMarkSeen(EmailFilter
+                .filter().flag(Flags.Flag.SEEN, false));
+
+        // If there are any emails loop through them displaying their contents
+        // and saving the attachments.
+        if (emails != null) {
+            for (ReceivedEmail email : emails) {
+                log.info("\n\n===[" + email.getMessageNumber() + "]===");
+                // common info
+                log.info("FLAG: %1$h\n", email.getFlags());
+                log.info("FROM:" + email.getFrom());
+                log.info("TO:" + email.getTo()[0]);
+                log.info("SUBJECT:" + email.getSubject());
+                log.info("PRIORITY:" + email.getPriority());
+                log.info("SENT DATE:" + email.getSentDate());
+                log.info("RECEIVED DATE: " + email.getReceiveDate());
+
+                // Messages may be multi-part so they are stored in an array
+                List<EmailMessage> messages = email.getAllMessages();
+                for (EmailMessage msg : messages) {
+                    log.info("------");
+                    log.info(msg.getEncoding());
+                    log.info(msg.getMimeType());
+                    log.info(msg.getContent());
+                }
+
+                // There may be multiple arrays so they are stored in an array
+                List<EmailAttachment> attachments = email.getAttachments();
+                if (attachments != null) {
+                    log.info("+++++");
+                    for (EmailAttachment attachment : attachments) {
+                        log.info("name: " + attachment.getName());
+                        log.info("cid: " + attachment.getContentId());
+                        log.info("size: " + attachment.getSize());
+                        // Write the file to disk
+                        // Location hard coded in this example
+                        attachment.writeToFile(new File(attachmentFolder, attachment.getName()));
+                    }
+                }
+            }
+        }
+        session.close();
+    }
+
+    /**
+     * Here we create an email message that contains html, embedded image, and
+     * an attachment
+     *
+     * @throws Exception In case we don't find the file to attach/embed
+     */
+    public void sendWithEmbedded(String subject, String content, String fileEmbeddedPath) throws Exception {
+
+        // Create am SMTP server object
+        SmtpServer<SmtpSslServer> smtpServer = SmtpSslServer
+                .create(smtpServerName).authenticateWith(emailSend,
+                emailSendPwd);
+
+        smtpServer.debug(true);
+
+        // Using the fluent style of coding create an html message
+        Email email = Email.create().from(emailSend)
+                .to(emailReceive)
+                .subject(subject)
+                .addText(content)
+                .embed(EmailAttachment.attachment()
+                        .bytes(new File(fileEmbeddedPath)));
+
+        // A session is the object responsible for communicating with the server
+        SendMailSession session = smtpServer.createSession();
+
+        // Like a file we open the session, send the mesage and close the
+        // session
+        session.open();
+        session.sendMail(email);
+        session.close();
+
+    }
+
+    public void sendWithAttachment(String subject, String content,
+            String fileAttachmentPath) throws Exception {
+
+        // Create am SMTP server object
+        SmtpServer<SmtpSslServer> smtpServer = SmtpSslServer
+                .create(smtpServerName).authenticateWith(emailSend,
+                emailSendPwd);
+
+        smtpServer.debug(true);
+
+        // Using the fluent style of coding create an html message
+        Email email = Email.create().from(emailSend)
+                .to(emailReceive)
+                .subject(subject)
+                .addText(content)
+                .attach(EmailAttachment.attachment().file(fileAttachmentPath));
+
+        // A session is the object responsible for communicating with the server
+        SendMailSession session = smtpServer.createSession();
+
+        // Like a file we open the session, send the mesage and close the
+        // session
+        session.open();
+        session.sendMail(email);
+        session.close();
 
     }
 
@@ -201,8 +326,33 @@ public class Mail {
      *
      * @throws Exception In case we don't find the file to attach/embed
      */
-    public void sendWithEmbeddedAndAttachment() throws Exception {
+    public void sendWithEmbeddedAndAttachment(String subject, String content,
+            String fileAttachmentPath, String fileEmbededPath) throws Exception {
 
+        // Create am SMTP server object
+        SmtpServer<SmtpSslServer> smtpServer = SmtpSslServer
+                .create(smtpServerName).authenticateWith(emailSend,
+                emailSendPwd);
+
+        smtpServer.debug(true);
+
+        // Using the fluent style of coding create an html message
+        Email email = Email.create().from(emailSend)
+                .to(emailReceive)
+                .subject(subject)
+                .addText(content)
+                .embed(EmailAttachment.attachment()
+                        .bytes(new File(fileEmbededPath)))
+                .attach(EmailAttachment.attachment().file(fileAttachmentPath));
+
+        // A session is the object responsible for communicating with the server
+        SendMailSession session = smtpServer.createSession();
+
+        // Like a file we open the session, send the mesage and close the
+        // session
+        session.open();
+        session.sendMail(email);
+        session.close();
     }
 
 }
